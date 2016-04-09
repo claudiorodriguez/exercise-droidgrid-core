759b7bf1c5d659cae466d3acb4f1a06dabf642d6
Claudio Rodriguez <cjrodr@yahoo.com>
2016-04-09 15:28:57 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# exercise-droidgrid-core
-Core module for Droidgrid exercise.
+# rgba-to-datauri
+
+[![Build Status][travis-image]][travis-url]
+
+Performs droid placement and movement operations on a 2D grid, checking for collision.
+
+## Installation
+
+### node.js
+
+Install using [npm](http://npmjs.org/):
+
+```bash
+$ npm install git+https://github.com/claudiorodriguez/exercise-droidgrid-core.git
+```
+
+## Examples
+
+**Node.JS**
+
+```javascript
+const DroidGrid = require('droidgrid');
+
+const opts = { width: 6, height: 6 };
+let grid = new DroidGrid(opts);
+grid.placeDroid(1, 2, 'north'); // x, y, facing (north|south|east|west)
+grid.moveDroid(['left', 'move', 'right', 'move']); // array of left|right|move
+grid.placeDroid(1, 3, 'north'); // droids are placed sequentially
+// you will keep moving the same droid in consecutive moveDroid calls:
+grid.moveDroid(['left', 'right', 'left']);
+grid.moveDroid('move'); // this will throw an error (collision)
+grid.placeDroid(0, 3, 'east'); // this will also throw an error (occupied)
+
+// gets a dump of the grid's current state in object form
+let snapshot = grid.snapshot();
+// create a grid from a snapshot:
+let anotherGrid = new DroidGrid(snapshot);
+```
+
+## Testing
+
+To run the tests:
+
+```bash
+$ npm test
+```
+
+## Contributing
+
+This is an exercise project. You are welcome to fork it but I am unlikely to maintain it.
+
+## License
+
+MIT - see [LICENSE][license-url]
+
+[travis-image]: https://travis-ci.org/claudiorodriguez/exercise-droidgrid-core.svg?branch=master
+[travis-url]: https://travis-ci.org/claudiorodriguez/exercise-droidgrid-core
+[license-url]: https://github.com/claudiorodriguez/exercise-droidgrid-core/blob/master/LICENSE
---
+++ test/basic.js
@@ -0,0 +1,84 @@
+'use strict';
+
+const chai = require('chai'),
+    rgbaToDataUri = require('../index');
+
+const expect = chai.expect,
+    assert = chai.assert;
+
+// ----- Basic tests ----- //
+
+const DroidGrid = require('../index');
+
+exports['Test placement and movement'] = {
+  // This is the provided spec
+  'two droids no collision': function(done) {
+    const expectedDroids = [
+      {x: 1, y: 3, facing: 'north'},
+      {x: 5, y: 1, facing: 'east'}
+    ];
+
+    const opts = { width: 6, height: 6 };
+    let grid = new DroidGrid(opts);
+
+    function placeAndMove() {
+      grid.placeDroid(1, 2, 'north');
+      grid.moveDroid(['left', 'move', 'left', 'move', 'left', 'move', 'left', 'move', 'move']);
+
+      grid.placeDroid(3, 3, 'east');
+      grid.moveDroid(['move', 'move', 'right', 'move', 'move', 'right', 'move', 'right', 'right', 'move']);
+    }
+
+    expect(placeAndMove).to.not.throw(Error);
+
+    let snapshot = grid.snapshot();
+
+    expect(snapshot.droids).to.exist;
+    expect(snapshot.droids).to.eql(expectedDroids);
+
+    done();
+  },
+
+  'moving collision': function(done) {
+    const opts = { width: 6, height: 6 };
+    let grid = new DroidGrid(opts);
+
+    function placeAndMove() {
+      grid.placeDroid(1, 1, 'north');
+      grid.moveDroid(['left', 'move']);
+
+      grid.placeDroid(1, 1, 'west');
+      grid.moveDroid(['move']);
+    }
+
+    expect(placeAndMove).to.throw(Error);
+
+    done();
+  },
+
+  'placement collision': function(done) {
+    const opts = { width: 6, height: 6 };
+    let grid = new DroidGrid(opts);
+
+    function place() {
+      grid.placeDroid(1, 1, 'north');
+      grid.placeDroid(1, 1, 'west');
+    }
+
+    expect(place).to.throw(Error);
+
+    done();
+  },
+
+  'invalid grid size': function(done) {
+    const opts = { width: 6, height: -1 };
+
+    function newBadGrid() {
+      let grid = new DroidGrid(opts);
+    }
+
+    expect(newBadGrid).to.throw(Error);
+
+    done();
+  }
+};
